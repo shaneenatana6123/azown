193af5e049c431ccc0226538703c73b704f47a2b
shane6123 <90815651+shane6123@users.noreply.github.com>
2023-04-03 15:59:29 +0530

    update user type in user collection vis-0 ven-1 bro-2 buld-3 own-4

--- server/controller/usercontroller.js
+++ server/controller/usercontroller.js
@@ -6,7 +6,7 @@ const JWT_SECRET = "shaneisgoodboy";
 const { uploadFile, getObjectSignedUrl, deleteFile } = require("../Storage/s3");
 const crypto = require("crypto");
 const { findOne } = require("../models/vender_service");
-const { vender_kyc, broker_kyc, user_kyc } = require("../models/kyc");
+const { vender_kyc, broker_kyc, builder_kyc, owner_kyc } = require("../models/kyc");
 const user_template = require("../models/user_template");
 const generateFileName = (bytes = 32) =>
   crypto.randomBytes(bytes).toString("hex");
@@ -44,7 +44,7 @@ const createuser = async (req, res) => {
       },
     };
     const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({user, authtoken });
+    res.json({ user, authtoken });
   } catch (error) {
     // console.error(error.message);
     res.status(500).json({ error: "Internal Server Error" });
@@ -73,7 +73,7 @@ const login = async (req, res) => {
     };
     const authtoken = jwt.sign(data, JWT_SECRET);
     success = true;
-    return res.json({ user,authtoken });
+    return res.json({ user, authtoken });
   } catch {
     // console.error(error.message);
     res.status(500).json({ error: "Internal Server Error" });
@@ -138,9 +138,7 @@ const kycupdate = async (req, res) => {
   }
 };
 
-
 const updateUserData = async (req, res) => {
-
   try {
     const file = req.file;
     if (file) {
@@ -155,16 +153,16 @@ const updateUserData = async (req, res) => {
     if (!user) {
       return res.status(400).json({ error: "User not exist" });
     }
-      console.log(user);
-      console.log(user._id);
-    const update = await User.findOneAndUpdate({_id: user._id}, req.body, {
+    console.log(user);
+    console.log(user._id);
+    const update = await User.findOneAndUpdate({ _id: user._id }, req.body, {
       new: true,
     });
     console.log(update);
-    return res.json({  message: "User Profile updated successfully" });
+    return res.json({ message: "User Profile updated successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error:error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -182,38 +180,41 @@ const getuserdetail = async (req, res) => {
 
 const addVenderKyc = async (req, res) => {
   try {
-    const { vender_aadhar , vender_pan , vender_gst } = req.body;
+    const { vender_aadhar, vender_pan, vender_gst } = req.body;
     // console.log(req.body);
 
     const vender_id = req.user.id;
-    const vender = await User.findOne({ vender_id });
+    const vender = await vender_kyc.findOne({ _id: vender_id });
+
     if (vender) {
       return res.status(400).json({
         error: "Sorry already kyc is done for this vender kindly update it.",
       });
     }
+   
     const venderKycData = await vender_kyc.create({
       vender_id,
       vender_aadhar,
       vender_pan,
       vender_gst,
     });
+    const update = await User.updateOne(
+      { _id: req.user.id },
+      { usertype: "1" }
+    );
     res.status(201).json({ message: "Vender Kyc Added Successfully" });
   } catch (error) {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-
-}
-
+};
 
 const updateVenderKyc = async (req, res) => {
   try {
-
-      const updateData =  await vender_kyc.updateOne(
+    const updateData = await vender_kyc.updateOne(
       { vender_id: req.user.id },
       {
-        $set:req.body
+        $set: req.body,
       }
     );
     res.status(201).json({ message: "Vender Kyc Updated Successfully" });
@@ -221,13 +222,11 @@ const updateVenderKyc = async (req, res) => {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-
-}
-
+};
 
 const addBrokerKyc = async (req, res) => {
   try {
-    const { broker_aadhar , broker_pan , broker_gst } = req.body;
+    const { broker_aadhar, broker_pan, broker_gst } = req.body;
     // console.log(req.body);
     const broker_id = req.user.id;
     const broker = await broker_kyc.findOne({ broker_id });
@@ -242,21 +241,23 @@ const addBrokerKyc = async (req, res) => {
       broker_pan,
       broker_gst,
     });
+    const update = await User.updateOne(
+      { _id: req.user.id },
+      { usertype: "2" }
+    );
     res.status(201).json({ message: "Broker Kyc Added Successfully" });
   } catch (error) {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-
-}
-
+};
 
 const updateBrokerKyc = async (req, res) => {
   try {
     const brokerUpdateData = await broker_kyc.updateOne(
       { broker_id: req.user.id },
       {
-        $set:req.body
+        $set: req.body,
       }
     );
     res.status(201).json({ message: "Broker Kyc Updated Successfully" });
@@ -264,55 +265,12 @@ const updateBrokerKyc = async (req, res) => {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
-
-
-
-const addUserKyc = async (req, res) => {
-  try {
-    const { user_aadhar , user_pan , user_gst } = req.body;
-    // console.log(req.body);
-    const user_id = req.user.id;
-    const user = await user_kyc.findOne({ id: user_id});
-    if (user) {
-      return res.status(400).json({
-        error: "Sorry already kyc is done for this user kindly update it.",
-      });
-    }
-    const userKycData = await user_kyc.create({
-      user_id,
-      user_aadhar,
-      user_pan,
-      user_gst,
-    });
-
-    res.status(201).json({ message: "User Kyc Added Successfully" });
-  } catch (error) {
-    // console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
 };
-
-
-const updateUserKyc = async (req, res) => {
-  try {
-    const userUpdateData = await user_kyc.updateOne(
-      { user_id: req.user.id },
-      {
-        $set:req.body
-      }
-    );
-    res.status(201).json({ message: "User Kyc Updated Successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-}
-
 
 const addTemplateData = async (req, res) => {
   try {
     const user_id = req.user.id;
-  
+
     const { template_data } = req.body;
     console.log(req.body);
     const template = await user_template.create({
@@ -330,10 +288,7 @@ const updateTemplateData = async (req, res) => {
   try {
     const user_id = req.user.id;
     const { template_data } = req.body;
-    const template = await user_template.updateOne(
-      { user_id },
-      req.body
-    );
+    const template = await user_template.updateOne({ user_id }, req.body);
     res.status(201).json({ message: "Template Updated Successfully" });
   } catch (error) {
     // console.error(error);
@@ -341,18 +296,16 @@ const updateTemplateData = async (req, res) => {
   }
 };
 
-
 const fetchTemplateData = async (req, res) => {
   try {
     const user_id = req.user.id;
-      const template_data = await user_template.findOne({ user_id });
-      res.status(201).json({ template_data });
+    const template_data = await user_template.findOne({ user_id });
+    res.status(201).json({ template_data });
   } catch (error) {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-
 
 const fetchVenderKyc = async (req, res) => {
   try {
@@ -376,13 +329,85 @@ const fetchBrokerKyc = async (req, res) => {
   }
 };
 
-const fetchUserKyc = async (req, res) => {
+const addBuilderKyc = async (req, res) => {
   try {
-    const user_id = req.user.id;
-    const user_kyc_data = await user_kyc.findOne({
-      user_id,
+    const { builder_aadhar, builder_pan, builder_gst } = req.body;
+    // console.log(req.body);
+    const builder_id = req.user.id;
+    const user = await builder_kyc.findOne({ builder_id });
+    if (user) {
+      return res.status(400).json({
+        error: "Sorry already kyc is done for this user kindly update it.",
+      });
+    }
+    const builderKycData = await builder_kyc.create({
+      builder_id,
+      builder_aadhar,
+      builder_pan,
+      builder_gst,
     });
-    res.status(201).json({ user_kyc_data });
+    const update = await User.updateOne(
+      { _id: req.user.id },
+      { usertype: "3" }
+    );
+    res.status(201).json({ message: "Builder Kyc Added Successfully" });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateBuilderKyc = async (req, res) => {
+  try {
+    const builderUpdateData = await builder_kyc.updateOne(
+      { builder_id: req.user.id },
+      {
+        $set: req.body,
+      }
+    );
+    res.status(201).json({ message: "User Kyc Updated Successfully" });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const fetchBuilderKyc = async (req, res) => {
+  try {
+    const builder_id = req.user.id;
+    const builder_data = await builder_kyc.findOne({ builder_id });
+    res.status(201).json({ builder_data });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const addOwnerKyc = async (req, res) => {
+  try {
+    const { owner_aadhar, owner_pan, owner_gst } = req.body;
+    // console.log(req.body);
+    const owner_id = req.user.id;
+    const user = await owner_kyc.findOne({ owner_id });
+    if (user) {
+      return res.status(400).json({
+        error: "Sorry already kyc is done for this user kindly update it.",
+      });
+    }
+    const ownerKycData = await owner_kyc.create({
+      owner_id,
+      owner_aadhar,
+      owner_pan,
+      owner_gst,
+    });
+    const updateUser = await User.updateOne(
+      { _id: req.user.id },
+      {
+        usertype: "4",
+      }
+    );
+
+    res.status(201).json({ message: "Owner Kyc Added Successfully" });
   } catch (error) {
     // console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -390,4 +415,52 @@ const fetchUserKyc = async (req, res) => {
 };
 
 
-module.exports = { createuser, login, kycupdate, getuserdetail , updateUserData , addVenderKyc , updateVenderKyc , addBrokerKyc , updateBrokerKyc , addUserKyc , updateUserKyc   , addTemplateData , updateTemplateData , fetchTemplateData , fetchVenderKyc , fetchBrokerKyc , fetchUserKyc };
+const updateOwnerKyc = async (req, res) => {
+  try {
+    const ownerUpdateData = await owner_kyc.updateOne(
+      { owner_id: req.user.id },
+      {
+        $set: req.body,
+      }
+    );
+    res.status(201).json({ message: "Owner Kyc Updated Successfully" });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const fetchOwnerKyc = async (req, res) => {
+  try {
+    const owner_id = req.user.id;
+    const owner_data = await owner_kyc.findOne({ owner_id });
+    res.status(201).json({ owner_data });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+module.exports = {
+  createuser,
+  login,
+  kycupdate,
+  getuserdetail,
+  updateUserData,
+  addVenderKyc,
+  updateVenderKyc,
+  addBrokerKyc,
+  updateBrokerKyc,
+  addTemplateData,
+  updateTemplateData,
+  fetchTemplateData,
+  fetchVenderKyc,
+  fetchBrokerKyc,
+  addBuilderKyc,
+  updateBuilderKyc,
+  fetchBuilderKyc,
+  addOwnerKyc,
+  updateOwnerKyc,
+  fetchOwnerKyc,
+};
--- server/models/kyc.js
+++ server/models/kyc.js
@@ -35,21 +35,37 @@ const broker_kycSchema = new Schema({
 }, { timestamps: true });
 const broker_kyc = mongoose.model('broker_kyc', broker_kycSchema);
 
-const user_kycSchema = new Schema({
-    user_id:{
+const builder_kycSchema = new Schema({
+    builder_id:{
         type:String
     },
-    user_aadhar:{
+    builder_aadhar:{
         type:String
     },
-    user_pan:{
+    builder_pan:{
         type:String
     },
-    user_gst:{
+    builder_gst:{
         type:String
     },
 }, { timestamps: true });
-const user_kyc = mongoose.model('user_kyc', user_kycSchema);
+const builder_kyc = mongoose.model('builder_kyc', builder_kycSchema);
 
-module.exports =  {  vender_kyc  ,   broker_kyc  , user_kyc};
+const owner_kycSchema = new Schema({
+    owner_id:{
+        type:String
+    },
+    owner_aadhar:{
+        type:String
+    },
+    owner_pan:{
+        type:String
+    },
+    owner_gst:{
+        type:String
+    },
+}, { timestamps: true });
+const owner_kyc = mongoose.model('owner_kyc', owner_kycSchema);
+
+module.exports =  {  vender_kyc  ,   broker_kyc  ,owner_kyc , builder_kyc };
     
